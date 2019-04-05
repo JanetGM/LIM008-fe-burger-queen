588ecdc7f82be3bbd89a2ed4a4870a37b24eed16
JanetGM <antaly.146@gmail.com>
2019-04-04 20:13:26 -0500

    agregando convirtiendo en array  menu.json

--- src/App.js
+++ src/App.js
@@ -2,28 +2,28 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import firebase from 'firebase';
-import {DB_CONFIG} from './config/config';
-import Pedido from './Pedido/Pedido';
-import PedidoForm from './PedidoForm/PedidoForm';
-import Home from './Home/Home';
-import 'firebase/database';
+// import firebase from 'firebase';
+// import {DB_CONFIG} from './config/config';
+// import Pedido from './Pedido/Pedido';
+// import PedidoForm from './PedidoForm/PedidoForm';
+// import Home from './Home/Home';
+// import 'firebase/database';
 
 
 class App extends Component {
-constructor(){
-  super();
+constructor(props){
+  super(props);
   this.state = {
-    items:[ ],
+    items:[],
     isLoaded:false,
-  };
+  }
   
 }
 
 componentDidMount(){
   fetch('https://raw.githubusercontent.com/JanetGM/LIM008-fe-burger-queen/devJanet/src/database/menu.json')
   .then(res => res.json())
-  .then(json =>{
+  .then(json => {
     this.setState({
       isLoaded:true,
       items:json,
@@ -32,23 +32,24 @@ componentDidMount(){
 }
 
   render() {
-    let {isLoaded,items}=this.state;
-    if(!isLoaded){
+    let {isLoaded,items} = this.state;
+    if(!isLoaded) {
       return <div>Loading..hay problemas con la data beibi.</div>;
     } 
     else {
-  
     return (
       <div className="App">
         <ul>
-          {items.map(item => {
-            <li key={item.id}></li>
-          })};
+          {items.map(item => (
+            <li key = {item.id}>
+              {item.name}
+            </li>
+          )) };
         </ul>
       </div>
     );
   }
-}
+ }
 }
 
 export default App;
