4ef60d82bdc3562cfaa706ca28f6f2495b21a6a6
JanetGM <antaly.146@gmail.com>
2019-04-05 15:21:55 -0500

    modificando json y se muestra el menu pero junta

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Breakfast from './Breakfast/Breakfast';
 import './App.css';
 
-// import firebase from 'firebase';
-// import {DB_CONFIG} from './config/config';
-// import Pedido from './Pedido/Pedido';
-// import PedidoForm from './PedidoForm/PedidoForm';
-// import Home from './Home/Home';
-// import 'firebase/database';
+import firebase from 'firebase';
+import {DB_CONFIG} from './config/config';
+import Pedido from './Pedido/Pedido';
+import PedidoForm from './PedidoForm/PedidoForm';
+import Home from './Home/Home';
+import 'firebase/database';
 
 
 class App extends Component {
-constructor(props){
-  super(props);
+constructor(){
+  super();
   this.state = {
     items:[],
     isLoaded:false,
-  }
+  };
   
 }
 
 componentDidMount(){
   fetch('https://raw.githubusercontent.com/JanetGM/LIM008-fe-burger-queen/devJanet/src/database/menu.json')
   .then(res => res.json())
-  .then(json => {
+  .then(json =>{
     this.setState({
       isLoaded:true,
       items:json,
@@ -32,27 +32,19 @@ componentDidMount(){
 }
 
   render() {
-    let {isLoaded,items} = this.state;
-    if(!isLoaded) {
+    let {isLoaded,items}=this.state;
+    if(!isLoaded){
       return <div>Loading..hay problemas con la data beibi.</div>;
     } 
     else {
+  
     return (
       <div className="App">
-        <ul>
-          {items.map(item => (
-            <li key = {item.id}>
-              {item.precio}
-            </li>
-          )) }
-        </ul>
-        <ul>
-
-        </ul>
+        <Breakfast menuBreakfast={this.state.items}/>
       </div>
     );
   }
- }
+}
 }
 
 export default App;
---
+++ src/Items/Items.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export class Items extends Component {
+
+
+
+  render() {
+    let {name,price}=this.props.menu;
+    return (
+      <div>
+        <p>{name}</p>
+        <p>Precio: {price}</p>
+      </div>
+    )
+  }
+}
+
+export default Items;
