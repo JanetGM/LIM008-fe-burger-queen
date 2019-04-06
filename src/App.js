764a847a681193e098a4c985bf7ec188bfece5e6
JanetGM <antaly.146@gmail.com>
2019-04-06 12:26:14 -0500

    funcion filtrado ready con hooks

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React, { useEffect, useState } from 'react';
-import axios from "axios";
 import './App.css';
 import Aumentar from './pruebaHooks/Aumentar';
 
 
-const ApiMenu = () =>{
-  const [info,setInfo] =useState([]);
-  
-  useEffect(() =>{
-    fetch('')
-    .then()
-    .then()
-  })
+const ApiMenu = () => {
+const prove = () =>{
+
 }
+const [info,setInfo] = useState([]);  
+useEffect(() => {
+    fetch('https://raw.githubusercontent.com/JanetGM/LIM008-fe-burger-queen/devJanet/src/database/menu.json')
+    .then(resp => resp.json())
+    .then(data => setInfo(data.concat(info)))
+  },[]);
+
+return(
+  <div>{info.map((item,key)=>(
+    <div key ={key}>{item.name}</div>
+  ))}
+    <p>{info.filter(item=> {
+      return(item.id===1)
+    }).map(e=><p>{e.turn}</p>)
+    }</p>
+
+  </div>
+
+)
+}
+export default ApiMenu;
