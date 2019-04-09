93d5bfb847f8e6a238d8b0d57cc97eb09b207bff
JanetGM <antaly.146@gmail.com>
2019-04-09 09:15:09 -0500

    subiendo imagenes , prueba 7 ,mostrando imagenes con menu

--- src/components/LoadDataMenu.js
+++ src/components/LoadDataMenu.js
@@ -1,9 +1,9 @@
 import React ,{Component,useState} from 'react';
 const LoadDataMenu = ({ info, setInfo }) => info.map(e =>
-<div class="row">
-<div class="col">
+<div className="row fondo">
+<div  key={e.id} className="col">
+<img  src={e.image}  className="cardImage offset-1 "/>
 <p className="offset-1 cardMenu" key={e.id}>{e.name}</p>
-<img src={e.image} className="cardImage"/>
 </div>
 </div>);
 export default LoadDataMenu;
