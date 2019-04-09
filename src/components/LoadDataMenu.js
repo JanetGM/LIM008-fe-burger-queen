e33c20e3e672c51d5657beaf15c83fa520129d88
JanetGM <antaly.146@gmail.com>
2019-04-08 19:21:59 -0500

    subiendo imagenes , prueba 6 ,mostrando imagenes con menu

--- src/components/LoadDataMenu.js
+++ src/components/LoadDataMenu.js
@@ -3,6 +3,7 @@ const LoadDataMenu = ({ info, setInfo }) => info.map(e =>
 <div class="row">
 <div class="col">
 <p className="offset-1 cardMenu" key={e.id}>{e.name}</p>
+<img src={e.image} className="cardImage"/>
 </div>
 </div>);
 export default LoadDataMenu;
