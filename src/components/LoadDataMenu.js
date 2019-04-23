8e5be5ebe450b8bef937d5f1ad8aaaa7f3d2e6d7
JanetGM <antaly.146@gmail.com>
2019-04-23 07:51:54 -0500

    cambiando estilos y correción de propiedad precio en menu.json

--- src/components/LoadDataMenu.js
+++ src/components/LoadDataMenu.js
@@ -3,12 +3,14 @@ import PropTypes from 'prop-types';
 
 export const LoadDataMenu = ({ info, additem, pedido, option }) => 
   (info.map((item, indice) => (option === item.type ? ( 
-    <div key={item.id} className="container-flex" onClick={() => additem(item, pedido, indice)} data-testid={`${item.id}-btn`}>
-      <div>
-       <img src={item.image} className="cardImage offset-1" alt="cardimage" />
-        {item.name}
-       </div>
-    </div>)
+  <div className="card border-success mb-3" style={{ width : "10rem"}}  key={item.id}  onClick={() => additem(item, pedido, indice)} data-testid={`${item.id}-btn`}>
+  <div className="card-body text-success">
+    <h5 className="card-title">{item.name}</h5>
+    <img className="card-img-top" src={item.image} className="card-img-top img-icon"  alt="Card image cap" />
+  </div>
+  <div class="card-footer bg-transparent border-success">{`Precio : $ ${item.price}`}</div>
+</div>
+    )
   : null
 )));
 export default LoadDataMenu;
